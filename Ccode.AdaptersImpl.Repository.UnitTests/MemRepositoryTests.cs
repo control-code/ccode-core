db1cc3352f7c189b99f470e0577675bcb7c8a8bc
am <am.home@gmail.com>
2022-11-16 18:24:41 +0100

    Add unit tests for in memory repository

---
+++ Ccode.AdaptersImpl.Repository.UnitTests/MemRepositoryTests.cs
@@ -0,0 +1,98 @@
+using Ccode.AdaptersImpl.Repository.InMemory;
+using Ccode.Domain;
+
+namespace Ccode.AdaptersImpl.Repository.UnitTests
+{
+	public class MemRepositoryTests
+	{
+		private record RootState(string Data);
+
+		private class Root : AggregateRoot<RootState>
+		{
+			public Root(Guid id) : base(id, new RootState(string.Empty))
+			{
+			}
+
+			public void SetData(string newData)
+			{
+				State = State with { Data = newData };
+			}
+		}
+
+		private readonly Guid _initiatorId = Guid.NewGuid();
+		private readonly Guid _correlationId = Guid.NewGuid();
+		private readonly Context _context;
+
+		public MemRepositoryTests()
+		{
+			_context = new Context(_initiatorId, _correlationId);
+		}
+
+		[Fact]
+		public void AddEntity()
+		{
+			var r = new MemRepository<Root, RootState>();
+
+			var id = Guid.NewGuid();
+			var entity = new Root(id);
+
+			r.Add(entity, _context).Wait();
+
+			Assert.Equal(1, r.StateEvents.Count);
+			Assert.Equal(StateEventOperation.Add, r.StateEvents.First().Operation);
+			Assert.Equal(1, r.EntityStates.Count);
+		}
+
+		[Fact]
+		public void UpdateEntity()
+		{
+			var r = new MemRepository<Root, RootState>();
+
+			var id = Guid.NewGuid();
+			var entity = new Root(id);
+
+			r.Add(entity, _context).Wait();
+			entity.SetData("New data");
+			r.Update(entity, _context).Wait();
+
+			Assert.Equal(2, r.StateEvents.Count);
+			Assert.Equal(StateEventOperation.Add, r.StateEvents.First().Operation);
+			Assert.Equal(StateEventOperation.Update, r.StateEvents.Skip(1).First().Operation);
+			Assert.Equal(1, r.EntityStates.Count);
+		}
+
+		[Fact]
+		public void DeleteEntity()
+		{
+			var r = new MemRepository<Root, RootState>();
+
+			var id = Guid.NewGuid();
+			var entity = new Root(id);
+
+			r.Add(entity, _context).Wait();
+			r.Delete(entity, _context).Wait();
+
+			Assert.Equal(2, r.StateEvents.Count);
+			Assert.Equal(StateEventOperation.Add, r.StateEvents.First().Operation);
+			Assert.Equal(StateEventOperation.Delete, r.StateEvents.Skip(1).First().Operation);
+			Assert.Equal(0, r.EntityStates.Count);
+		}
+
+		[Fact]
+		public void GetEntity()
+		{
+			var r = new MemRepository<Root, RootState>();
+
+			var id = Guid.NewGuid();
+			var entityA = new Root(id);
+
+			r.Add(entityA, _context);
+			entityA.SetData("New data");
+			r.Update(entityA, _context);
+
+			var entityB = r.Get(id).Result;
+
+			Assert.True(entityA == entityB);
+		}
+	}
+}
--- Ccode.AdaptersImpl.Repository/InMemory/MemRepository.cs
+++ Ccode.AdaptersImpl.Repository/InMemory/MemRepository.cs
@@ -8,9 +8,11 @@ namespace Ccode.AdaptersImpl.Repository.InMemory
  	public class MemRepository<T, TState> : IRepository<T, TState> where T : class, IAggregateRoot<TState>
 	{
 		private List<StateEvent> _stateEvents = new List<StateEvent>();
+		private SortedList<Guid, T> _entities = new SortedList<Guid, T>();
 		private SortedList<Guid, TState> _entityStates = new SortedList<Guid, TState>();
 
 		public ICollection<StateEvent> StateEvents { get { return _stateEvents.ToImmutableList(); } }
+		public ICollection<TState> EntityStates { get { return _entityStates.Values.ToImmutableList(); } }
 
 		public Task Add(T root, Context context)
 		{
@@ -22,6 +24,7 @@ namespace Ccode.AdaptersImpl.Repository.InMemory
 			var e = new StateEvent(root.Id, StateEventOperation.Add, root.State);
 			_stateEvents.Add(e);
 			_entityStates.Add(root.Id, root.State);
+			_entities.Add(root.Id, root);
 			return Task.CompletedTask;
 		}
 
@@ -50,30 +53,21 @@ namespace Ccode.AdaptersImpl.Repository.InMemory
 				throw new NullReferenceException("entity.State");
 			}
 
-			var e = new StateEvent(root.Id, StateEventOperation.Add, root.State);
+			var e = new StateEvent(root.Id, StateEventOperation.Delete, root.State);
 			_stateEvents.Add(e);
 			_entityStates.Remove(root.Id);
+			_entities.Remove(root.Id);
 			return Task.CompletedTask;
 		}
 
 		public Task<T?> Get(Guid id)
 		{
-			if (!_entityStates.ContainsKey(id))
+			if (!_entities.ContainsKey(id))
 			{
 				return Task.FromResult((T?)null);
 			}
 
-			var state = _entityStates[id];
-
-			if (state == null)
-			{
-				return Task.FromResult((T?)null);
-			}
-
-			ConstructorInfo? ctor = typeof(T).GetConstructor(new[] { typeof(Guid), typeof(TState) });
-			object? instance = ctor?.Invoke(new object[] { id, state });
-
-			return Task.FromResult((T?)instance);
+			return Task.FromResult((T?)_entities[id]);
 		}
 	}
 }
